4152b09ca94aee0a09abec7932909f6688e7f74c
anureetsandhu <97444644+anureetsandhu@users.noreply.github.com>
2022-01-13 23:58:58 +0530

    Add files via upload

---
+++ Output assignment 1.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================================ RESTART: /Users/anureetsandhu/Downloads/1.py ============================================================================
+enter first number here:10
+enter second number here:20
+enter third number here:30
+average is: 20.0
+
+============================================================================ RESTART: /Users/anureetsandhu/Downloads/q2.py ===========================================================================
+enter your gross income50000
+Enter your number of dependents3
+Your income tax is :
+6200.0
+
+============================================================================ RESTART: /Users/anureetsandhu/Downloads/q2.py ===========================================================================
+Enter your SID here:21103017
+Enter your name here:ANUREET SANDHU
+Enter your gender hereF
+Enter your course name here:CSE
+Enter your CGPA here:9
+['21103017', 'ANUREET SANDHU', 'F', 'CSE', 9.0]
+
+============================================================================ RESTART: /Users/anureetsandhu/Downloads/q2.py ===========================================================================
+Enter marks of student98
+Enter marks of student87
+Enter marks of student95
+Enter marks of student100
+Enter marks of student89
+['100', '87', '89', '95', '98']
+
+============================================================================ RESTART: /Users/anureetsandhu/Downloads/q2.py ===========================================================================
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
