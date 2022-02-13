c31b3740a4dbbe9c9d2b40526b7ba66fb29ea95e
anureetsandhu <97444644+anureetsandhu@users.noreply.github.com>
2022-02-13 23:06:02 +0530

    Add files via upload

---
+++ 21103017_Assignment 3_output.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 1.py ====================================
+Q1
+Enter a string: turtles
+Ocuurences of characters in input string:  {'t': 2, 'u': 1, 'r': 1, 'l': 1, 'e': 1, 's': 1}
+
+
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 1.py ====================================
+Q1
+Enter a string: assignment three
+Occurences of words in input string:  {'assignment': 1, 'three': 1}
+
+
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 2.py ====================================
+Q2
+Enter the day:23
+Enter month:6
+Enter the year:2017
+Next date is: 24 / 6 / 2017
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 3.py ====================================
+Q3
+the list is [2, 5, 6, 23, 34]
+output: [(2, 4), (5, 25), (6, 36), (23, 529), (34, 1156)]
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 4.py ====================================
+Q4
+Enter your grade lying between 4 and 10:8
+Your grade is ' B+ ' and  Very Good  performance.
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 5.py ====================================
+Q5
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 6.py ====================================
+Q6
+Enter the name of the student: Anureet
+Enter the SID of Anureet: 17
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: abc
+Enter the SID of abc: 18
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: xyz
+Enter the SID of xyz: 19
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of Anureet is 17
+The SID of abc is 18
+The SID of xyz is 19
+
+b. Student Details (sorted with respect to names):
+The SID of Anureet is 17
+The SID of abc is 18
+The SID of xyz is 19
+
+c. Student Details (sorted with respect to SIDs):
+The SID of Anureet is 17
+The SID of abc is 18
+The SID of xyz is 19
+
+d. Enter the SID of the student: 17
+The name of student whose SID is 17 is Anureet
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 7.py ====================================
+Q7
+Enter the number of terms of the fibonacci sequence: 6
+0
+1
+1
+2
+3
+5
+The average of the above fibonacci series is:  2.0 
+
+
+===================================== RESTART: /Users/anureetsandhu/Documents/ques 8.py ====================================
+Q8
+{1, 2, 3, 4, 5}
+{8, 2, 4, 6}
+{1, 17, 5, 9, 13}
+a. set of elements that are in set 1 and set 2 but not both: {1, 3, 5, 6, 8}
+b. set of elements that are in only one of the three sets: {17, 3, 6, 8, 9, 13}
+c. set of elements that are in exactly two of the sets set1, set2, set3: {1, 2, 4, 5}
+d. set of all  integers in the range 1 to 10 that are not in set1: {8, 9, 6, 7}
+e. set of all integers  in the range 1 to 10 that arenot in set1,set2 ans set3: {7}
