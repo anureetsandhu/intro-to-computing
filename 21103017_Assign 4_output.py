60ac1964121b5621fb647d47874b11deb689671c
anureetsandhu <97444644+anureetsandhu@users.noreply.github.com>
2022-02-27 22:19:55 +0530

    Add files via upload

---
+++ 21103017_Assign 4_output.py
@@ -0,0 +1,79 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================= RESTART: /Users/anureetsandhu/Downloads/21103017_ASSIGNMENT 4.py =============================
+Q1
+Tower of Hanoi:
+Enter number of disks: 4
+Move disk 1 from A to B
+Move disk 2 from  A to  C
+Move disk 1 from B to C
+Move disk 3 from  A to  B
+Move disk 1 from C to A
+Move disk 2 from  C to  B
+Move disk 1 from A to B
+Move disk 4 from  A to  C
+Move disk 1 from B to C
+Move disk 2 from  B to  A
+Move disk 1 from C to A
+Move disk 3 from  B to  C
+Move disk 1 from A to B
+Move disk 2 from  A to  C
+Move disk 1 from B to C
+No. of Steps taken are: 15 = 2^n - 1
+
+Q2 using recursion
+Enter number of rows to form pascal triangle: 10
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+1 7 21 35 35 21 7 1 
+1 8 28 56 70 56 28 8 1 
+1 9 36 84 126 126 84 36 9 1 
+
+Q2 using for loop
+enter the no of rows required in Pascal's triangle:10
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+1 7 21 35 35 21 7 1 
+1 8 28 56 70 56 28 8 1 
+1 9 36 84 126 126 84 36 9 1 
+
+Q3
+Enter first integer value: 120
+Enter second integer value: 24
+a) function is callable: True
+b) all the values are non zero
+c) result= 5 , 0
+c) result along with added values: [5, 0, 4, 5, 6]
+c) result after filtering  out values greater than 4: 0 
+d) converted set: {0, 4, 5, 6}
+e) immutable  set: frozenset({0, 4, 5, 6})
+f) maximum value= 6
+6.0
+f) hash value= 6
+
+Q4
+Anureet
+21103017
+
+Object is destroyed
+
+Q5
+a)salary  of Mehak: 40000
+ salary of Mehak after updating: 70000
+
+b)deleting the record of employee Viren
+Q6
+Enter word uttered by George: cars
+Enter meaningful word uttered by Barbie: scar
+Friendship is real
